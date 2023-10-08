8a340485a08e077968e8c3b3c79d707bf2d033fd
namelessman <zhoumq.good@gmail.com>
2023-10-08 16:44:11 +1300

    feat: coordinator application

--- src/pages/coordinator/Application.jsx
+++ src/pages/coordinator/Application.jsx
@@ -1,56 +1,108 @@
-import React, {useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from "../../components/ui/select"
-import {cn} from "../../lib/utils";
-import SupervisorApplication from "../../components/supervisor/Application";
-import SupervisorMarkers from "../../components/supervisor/Markers";
-import SupervisorCourses from "../../components/supervisor/Courses";
-
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "../../components/ui/table"
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { MdRadioButtonUnchecked } from 'react-icons/md'
+import useSwr from "swr";
+import fetcher from '../../lib/fetcher';
+import { Button } from '../../components/ui/button';
 
 function Application(props) {
-    const [value,setValue] = useState("application")
+    const [currentCourseId, setCurrentCourseId] = useState();
+    const [currentCourse, setCurrentCourse] = useState();
 
-        
-    
+    const { data: courses, isLoading } = useSwr('/GetAllCourses', fetcher)
+
+    const { data: applications } = useSwr(['/GetApplicationsByCourse', currentCourseId], ([url, id]) => id && fetcher(`${url}/${id}`))
+
+
+    useEffect(() => {
+        setCurrentCourseId(courses?.[0]?.id)
+        setCurrentCourse(courses?.[0])
+    }, [courses?.[0]?.id]);
+
     return (
         <div className={'flex'}>
-            <div className={'basis-1/6 border-r-[1px] border-black h-screen flex justify-center pt-8'}>
-             
-                <Select  defaultValue={value} onValueChange={(value)=>{setValue(value)}}
-                >
-                
-                    <SelectTrigger className="w-[180px]">
-                        
-                        <SelectValue className='text-black' placeholder="Select a application" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup >
-                            <SelectLabel>Applications</SelectLabel>
-                            <SelectItem value="courses" > Courses</SelectItem>
-                            <SelectItem value="markers" > Makers</SelectItem>
-                            <SelectItem value="applications" > Applications</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-            </div>
-            
-            <div className={'basis-5/6'}>
+            <div className={'basis-1/6 border-r-[1px] border-black h-screen flex flex-col p-8'}>
+
                 {
-                    value === 'courses' ?
-                        <SupervisorCourses/> :
-                        value === "markers" ?
-                        <SupervisorMarkers/> :
-                            <SupervisorApplication />
+                    (courses || []).map(i => <div
+                        onClick={() => {
+                            setCurrentCourseId(i.id);
+                            setCurrentCourse(i)
+                        }}
+                        className={`mb-2 border-2 border-black text-center p-2 cursor-pointer rounded-sm ${i.id === currentCourseId ? 'bg-black' : 'bg-white'} ${i.id === currentCourseId ? 'text-white' : 'text-black'}`}
+                    >
+                        <div className='font-bold text-lg'>{i.courseName} {i.courseNumber}</div>
+                        <div className='text-sm'>Applications: {i.applications?.length}</div>
+                    </div>
+                    )
                 }
-                
+            </div>
+
+            <div className={'basis-5/6'}>
+                <div className='flex flex-col gap-3 p-4'>
+                    <h4 className={'text-3xl'}>
+                        Applications
+                    </h4>
+                    <div className={'flex w-full justify-between shadow-md p-4'}>
+                        <p className={'text-lg'}>{currentCourse?.courseName} {currentCourse?.courseNumber}</p>
+                        <p className={'text-lg'}>Marked {currentCourse?.enrolledStudents}/{currentCourse?.estimatedStudents}</p>
+                        <p className={'text-lg'}>Number of Application: {currentCourse?.applications?.length}</p>
+                    </div>
+
+                    <div>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Grade</TableHead>
+                                    <TableHead>Have Marked</TableHead>
+                                    <TableHead>Overseas</TableHead>
+                                    <TableHead>Stage</TableHead>
+                                    <TableHead>Maximum number of hours</TableHead>
+                                    <TableHead>recommend</TableHead>
+                                    <TableHead>documents</TableHead>
+                                    <TableHead>Operations</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {applications?.map((application) => (
+                                    <TableRow key={application.id}>
+                                        <TableCell >{application.user?.name}</TableCell>
+                                        <TableCell>{application.previousGrade}</TableCell>
+                                        <TableCell>{application.haveMarkedBefore ? <MdRadioButtonUnchecked /> : <AiFillCheckCircle />}</TableCell>
+                                        <TableCell >{application.user?.isOverseas ? 'Yes' : 'No'}</TableCell>
+                                        <TableCell >{application.user?.enrolmentDetail}</TableCell>
+                                        <TableCell>{application.user?.remainHours}</TableCell>
+                                        <TableCell>{application.isRecommanded ? <AiFillCheckCircle /> : <MdRadioButtonUnchecked />}</TableCell>
+                                        <TableCell>
+                                            <Button className="mb-2">CV</Button>
+                                            <Button>Academic Record</Button>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Button className="mb-2">Accept</Button>
+                                            <Button>Not Qualified</Button>
+                                        </TableCell>
+                                        {/* <TableCell>
+                                            <AiFillCheckCircle size={20} />
+                                        </TableCell>
+                                        <TableCell>
+                                            <MdRadioButtonUnchecked size={20} />
+                                        </TableCell> */}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
             </div>
         </div>
     );
