331e9307eb2799f067a8174519295b15af646c07
namelessman <zhoumq.good@gmail.com>
2023-10-10 22:38:47 +1300

    feat: add semester

--- src/pages/coordinator/Home.jsx
+++ src/pages/coordinator/Home.jsx
@@ -9,13 +9,40 @@ import useSwr from "swr";
 import fetcher from '../../lib/fetcher';
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '../../components/ui/select';
 import { getSemesterId } from '../../lib/utils';
+import Modal from '../../components/Modal';
+import { Input } from '../../components/ui/input';
+import request from '../../lib/request';
+import toast from 'react-hot-toast';
 
 function Home(props) {
     const navigate = useNavigate()
     const { userType } = getUser()
     const cacheSemesterId = getSemesterId();
     const { data: semesters, isLoading } = useSwr('/GetAllSemesters', fetcher)
+    const [showAddNewSemester, setShowNewSemester] = useState(false);
+    const [inputYear, setInputYear] = useState();
+    const [inputSemester, setInputSemester] = useState();
     const [currentSemester, setCurrentSemester] = useState(cacheSemesterId && +cacheSemesterId)
+
+    const onAddNewSemester = async () => {
+        console.log(inputSemester)
+        if(!inputYear || !inputSemester) {
+            toast.error('Please finish the form')
+            return;
+        }
+        try {
+            await request.post('NewSemester',{
+                "year": inputYear,
+                "semesterType": inputSemester
+            })
+            toast.success("uploaded sucessfully! 🚀🚀🚀")
+            setShowNewSemester(false)
+            setInputSemester();
+            setInputYear();
+        } catch (e) {
+            toast.error(e.message)
+        }
+    }
 
     useEffect(() => {
         if (userType === 'User') {
@@ -29,6 +56,7 @@ function Home(props) {
 
     return (
         <div className={'h-screen flex flex-col gap-10 items-center justify-center'}>
+            <Button onClick={() => setShowNewSemester(true)}>Add New Semester</Button>
             <span className='text-xl'>Please select semester</span>
             <Select defaultValue={currentSemester} onValueChange={(value) => { setCurrentSemester(value) }}
             >
@@ -69,6 +97,31 @@ function Home(props) {
                 </Link>
             </div>
 
+            <Modal
+                isOpen={showAddNewSemester}
+                title="Add New Semester"
+                actionLabel='Save'
+                onClose={() => {setShowNewSemester(false)}}
+                onSubmit={onAddNewSemester}
+                body={<div className='text-center'>
+                    <Input 
+                        type="number"
+                        value={inputYear}
+                        onChange={e => setInputYear(e.target.value)}
+                        placeholder="Please input the semester year"
+                        className="mb-4"
+                    />
+                    <Input 
+                        value={inputSemester}
+                        onChange={e => setInputSemester(e.target.value)}
+                        placeholder="Please input the semester name"
+                    />
+                    <Button 
+                    onClick={onAddNewSemester}
+                        className="m-4 width-40"
+                        >Submit</Button>
+                </div>}
+            />
         </div>
     );
 }
