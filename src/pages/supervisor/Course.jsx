c9fb7e2f85fccdb39a002d02adb518786064342d
ThisIsAnAvailableName <110003231+ThisIsAnAvailableName@users.noreply.github.com>
2023-10-20 13:05:39 +1300

    Update Course.jsx

--- src/pages/supervisor/Course.jsx
+++ src/pages/supervisor/Course.jsx
@@ -68,9 +68,7 @@ function Courses(props) {
         <div className='p-6'>
             <EditCourseModal id={editId} onSuccess={onSuccess}/>
             <CreateCourseModal />
-            <Button className="mt-6" onClick={() => { createCourseModal.onOpen() }}>
-                Add Course
-            </Button>
+            
             <Table className="mt-6">
                 <TableHeader>
                     <TableRow>
